0534aad320e757fc72f41bf45853b48d59561099
Ryan Magowan <ryanm@nulogy.com>
2016-06-10 16:01:42 -0400

    migrates database to add table for skeletons

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160610195933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skeletons", force: :cascade do |t|
+    t.string "data"
+  end
 
 end
