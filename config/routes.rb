f021d73a9536be4a2877495383051ffa2d5a837e
Ryan Magowan <ryanm@nulogy.com>
2016-06-13 11:27:08 -0400

    follows suggested devise install steps post devise:install command

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'skeleton#index'
+
   resources :skeleton, only: [:index, :new, :create, :show]
 end
