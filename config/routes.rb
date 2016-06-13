2df38cb598e26de69c2420b9b315012ab098e600
Ryan Magowan <ryanm@nulogy.com>
2016-06-13 15:04:04 -0400

    adds devise controllers and views, adds tests and routes, mutes tests that now require authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    unlocks: 'users/unlocks',
+    # omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'skeleton#index'
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,10 @@ RSpec.configure do |config|
     Warden.test_mode!
   end
 
+  config.after :each do
+    Warden.test_reset!
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
