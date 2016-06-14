f8fbf2791a3d12c03941638a7d31b995c60f5feb
Ryan Magowan <ryanm@nulogy.com>
2016-06-14 16:58:25 -0400

    adds controller for bands and views for new and show

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,27 @@
+class BandsController < ApplicationController
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+
+    if @band.save
+      flash[:notice] = "You have added a Band to your festival"
+      redirect_to band_path(@band)
+    else
+      flash[:alert] = "There was a problem creating your band"
+      render :new
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:id, :name, :start_time, :end_time, :description, :website_url, :soundcloud_url)
+  end
+end
