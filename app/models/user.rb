691b1f844b2e23d0a036597c2bd58adc76bc4af8
Ryan Magowan <ryanm@nulogy.com>
2016-06-14 14:29:30 -0400

    adds role type to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :manager, :admin]
+  before_create { set_default_role }
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
