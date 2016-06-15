63751e08f424bf66f9bdadc7d6d1e379f1aa9082
Ryan Magowan <ryanm@nulogy.com>
2016-06-15 11:30:40 -0400

    validates uniqueness of festival title

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,2 +1,3 @@
 class Festival < ApplicationRecord
+  validates :title, uniqueness: true
 end
--- spec/models/festival_spec.rb
+++ spec/models/festival_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Festival, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates that Festival.title is unique" do
+    create_festival(title: "Two of the same")
+
+    expect{ create_festival(title: "Two of the same") }.to raise_error(ActiveRecord::RecordInvalid, /Title has already been taken/)
+  end
+
+  def create_festival(title: "festival_title", location: "fesival_location", website_url: "http://festival_website.com", start_time: Time.zone.now, end_time: Time.zone.now + 3.days)
+    Festival.create!( title: title,
+      location: location,
+      website_url: website_url,
+      start_time: start_time,
+      end_time: end_time
+      )
+  end
 end
