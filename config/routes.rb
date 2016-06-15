5f3a45f1b53f2181f4c0f1e8845f8e3ea36f2f8b
Ryan Magowan <ryanm@nulogy.com>
2016-06-15 10:29:51 -0400

    adds routes for edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :festivals, only: [:new, :create, :show]
-  resources :bands, only: [:new, :create, :show]
+  resources :bands, only: [:new, :create, :show, :edit, :update]
 end
