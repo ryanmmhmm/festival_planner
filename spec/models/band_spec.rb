68276562f498687b20c7f4a2bc01e89c63b48ee8
Ryan Magowan <ryanm@nulogy.com>
2016-06-16 16:52:27 -0400

    refactors create method name to use bang

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Band, :type => :model do
   it "valides that end_time > start_time" do
-    expect{ create_band(name: "Timewarp", start_time: Time.zone.now + 1.hour, end_time: Time.zone.now) }.to raise_error(ActiveRecord::RecordInvalid, /End time needs to be greater than start time/)
+    expect{ create_band!(name: "Timewarp", start_time: Time.zone.now + 1.hour, end_time: Time.zone.now) }.to raise_error(ActiveRecord::RecordInvalid, /End time needs to be greater than start time/)
   end
 
-  def create_band(name: "band_name",
+  def create_band!(name: "band_name",
     description: "band_description",
     website_url: "http://band_website.com",
     soundcloud_url: "http://soundcloud.com/band_account",
