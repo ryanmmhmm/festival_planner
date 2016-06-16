6e9c17fe025465e131fb4f5c482c28bd21c3f502
Ryan Magowan <ryanm@nulogy.com>
2016-06-16 16:49:16 -0400

    refactors test to used better verbage on create methods

--- spec/models/festival_spec.rb
+++ spec/models/festival_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Festival, :type => :model do
   it "validates that Festival.title is unique" do
     create_festival(title: "Two of the same")
 
-    expect{ create_festival(title: "Two of the same") }.to raise_error(ActiveRecord::RecordInvalid, /Title has already been taken/)
+    expect{ create_festival!(title: "Two of the same") }.to raise_error(ActiveRecord::RecordInvalid, /Title has already been taken/)
   end
 
   describe "end_time & start_time" do
     it "validates that start_time is present" do
-      festival = create_festival_no_bang(start_time: nil)
+      festival = create_festival(start_time: nil)
 
       validation = festival.valid?
 
@@ -17,7 +17,7 @@ RSpec.describe Festival, :type => :model do
     end
 
     it "validates that end_time is present" do
-      festival = create_festival_no_bang(end_time: nil)
+      festival = create_festival(end_time: nil)
 
       validation = festival.valid?
 
@@ -25,11 +25,11 @@ RSpec.describe Festival, :type => :model do
     end
 
     it "custom validates that end_time > start_time" do
-      expect{ create_festival(title: "Timewarp", start_time: Time.zone.now + 1.hour, end_time: Time.zone.now) }.to raise_error(ActiveRecord::RecordInvalid, /End time needs to be greater than start time/)
+      expect{ create_festival!(title: "Timewarp", start_time: Time.zone.now + 1.hour, end_time: Time.zone.now) }.to raise_error(ActiveRecord::RecordInvalid, /End time needs to be greater than start time/)
     end
   end
 
-  def create_festival(title: "festival_title", location: "fesival_location", website_url: "http://festival_website.com", start_time: Time.zone.now, end_time: Time.zone.now + 3.days)
+  def create_festival!(title: "festival_title", location: "fesival_location", website_url: "http://festival_website.com", start_time: Time.zone.now, end_time: Time.zone.now + 3.days)
     Festival.create!( title: title,
       location: location,
       website_url: website_url,
@@ -38,7 +38,7 @@ RSpec.describe Festival, :type => :model do
       )
   end
 
-  def create_festival_no_bang(title: "festival_title", location: "fesival_location", website_url: "http://festival_website.com", start_time: Time.zone.now, end_time: Time.zone.now + 3.days)
+  def create_festival(title: "festival_title", location: "fesival_location", website_url: "http://festival_website.com", start_time: Time.zone.now, end_time: Time.zone.now + 3.days)
     Festival.create( title: title,
       location: location,
       website_url: website_url,
