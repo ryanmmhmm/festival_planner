29259f8c0b6ea9856cae743d6bdbc394002745c7
Ryan Magowan <ryanm@nulogy.com>
2016-06-16 15:36:33 -0400

    changes controller methods to include the festival

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -4,35 +4,44 @@ class BandsController < ApplicationController
   end
 
   def new
-    @band = Band.new
+    @festival = Festival.find(params[:festival_id])
+    @band = @festival.bands.new
   end
 
   def create
-    @band = Band.new(band_params)
+    @festival = Festival.find(params[:festival_id])
+    @band = @festival.bands.new(band_params)
 
     if @band.save
       flash[:notice] = "You have added a Band to your festival"
-      redirect_to band_path(@band)
+      redirect_to festival_band_path(@festival, @band)
     else
-      flash[:alert] = "There was a problem creating your band"
+      if @band.errors.any?
+        flash[:alert] = @band.errors.full_messages
+      end
       render :new
     end
   end
 
   def show
+    @festival = Festival.find(params[:festival_id])
     @band = Band.find(params[:id])
+    puts "Band ID: #{@band.id}"
+    puts "Festival ID: #{@band.festival_id}"
   end
 
   def edit
+    @festival = Festival.find(params[:festival_id])
     @band = Band.find(params[:id])
   end
 
   def update
-    band = Band.find(params[:id])
+    @festival = Festival.find(params[:festival_id])
+    @band = Band.find(params[:id])
 
-    if band.update(band_params)
+    if @festival.bands.update(band_params)
       flash[:notice] = "Your changes have been saved"
-      redirect_to band_path(band)
+      redirect_to festival_band_path(@festival, @band)
     else
       flash[:alert] = "Something went wrong when updating your changes"
       render :edit
@@ -44,7 +53,7 @@ class BandsController < ApplicationController
 
     if band.destroy
       flash[:notice] = "Your band has been removed successfully"
-      redirect_to bands_path
+      redirect_to festival_bands_path(band.festival_id)
     else
       flash[:alert] = "Your band could not be deleted"
       render :show
@@ -54,6 +63,6 @@ class BandsController < ApplicationController
   private
 
   def band_params
-    params.require(:band).permit(:id, :name, :start_time, :end_time, :description, :website_url, :soundcloud_url)
+    params.require(:band).permit(:id, :festival_id, :name, :start_time, :end_time, :description, :website_url, :soundcloud_url)
   end
 end
