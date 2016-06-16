64074364951244fb0786b3e9eb8f39fd88300c71
Ryan Magowan <ryanm@nulogy.com>
2016-06-16 16:53:00 -0400

    adds create method without bang

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe Band, :type => :model do
       end_time: end_time
       )
   end
+
+  def create_band(name: "band_name",
+    description: "band_description",
+    website_url: "http://band_website.com",
+    soundcloud_url: "http://soundcloud.com/band_account",
+    start_time: Time.zone.now,
+    end_time: Time.zone.now + 1.hour
+    )
+
+    Band.create(name: name,
+      description: description,
+      website_url: website_url,
+      soundcloud_url: soundcloud_url,
+      start_time: start_time,
+      end_time: end_time
+      )
+  end
 end
